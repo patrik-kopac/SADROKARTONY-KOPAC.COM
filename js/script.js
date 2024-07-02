b6f95f4085e1833a944c402a6388039d66b183d7
Patrik Kopáč <kopacpatrik@email.cz>
2024-07-02 20:02:02 +0200

    dorobenie komplet, zabudol som gitovať, soráč

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// ------HAMBURGER MENU--------
+
+// tvorba premenných
+const menuIcon = document.querySelector(`.menu-icon`)
+
+const menuList = document.querySelector("nav")
+
+const hamburgerIcon = document.querySelector(".fa-solid")
+
+// prepínanie classov z hamburger menu na krížik a naopak
+menuIcon.addEventListener(`click`, () => {
+    if (hamburgerIcon.classList[1] === "fa-bars") {
+        hamburgerIcon.classList.toggle("fa-xmark")
+        hamburgerIcon.classList.remove("fa-bars")
+        menuList.style.display = "block";
+    } else {
+        hamburgerIcon.classList.add("fa-bars")
+        hamburgerIcon.classList.remove("fa-xmark");
+        menuList.style.display = "none"
+    }
+})
